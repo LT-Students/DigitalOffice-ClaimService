44b35672bf7050d7d2231fef63c179f304979f96
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2023-07-12 11:17:15 +0300

    add department id to claim

--- src/ClaimService.Business/Features/Claims/Commands/Create/CreateClaimValidator.cs
+++ src/ClaimService.Business/Features/Claims/Commands/Create/CreateClaimValidator.cs
@@ -1,14 +1,18 @@
 ﻿using FluentValidation;
+using LT.DigitalOffice.ClaimService.Broker.Requests.Interfaces;
 using LT.DigitalOffice.ClaimService.Business.Features.Claims.Commands.Create;
 using LT.DigitalOffice.ClaimService.DataLayer;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 namespace LT.DigitalOffice.ClaimService.Validation.Claim;
 
 public class CreateClaimValidator : AbstractValidator<CreateClaimCommand>
 {
-  public CreateClaimValidator(IDataProvider provider)
+  public CreateClaimValidator(
+    IDataProvider provider,
+    IDepartmentService departmentService)
   {
     RuleFor(r => r.Priority)
       .IsInEnum()
@@ -21,8 +25,18 @@ public class CreateClaimValidator : AbstractValidator<CreateClaimCommand>
         .WithMessage("DeadLine must not be earlier than now.");
     });
 
-    RuleFor(r => r.CategoryId)
-      .MustAsync((id, ct) => provider.Categories.AnyAsync(c => c.Id == id && c.IsActive, ct))
-      .WithMessage("No category with provided id was found.");
+    When(r => r.CategoryId.HasValue, () =>
+    {
+      RuleFor(r => r.CategoryId)
+        .MustAsync((id, ct) => provider.Categories.AnyAsync(c => c.Id == id && c.IsActive, ct))
+        .WithMessage("No category with provided id was found.");
+    });
+
+    When(r => r.DepartmentId.HasValue, () =>
+    {
+      RuleFor(r => r.DepartmentId)
+        .MustAsync((id, ct) => departmentService.DoesDepartmentExist(new List<Guid> { id.Value }))
+        .WithMessage("No department with provided id was found.");
+    });
   }
 }
